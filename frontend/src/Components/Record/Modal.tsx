b32d9c3799814f97f478d1bf8cf328691abc8042
VisH317 <54584201+VisH317@users.noreply.github.com>
2023-06-20 11:52:15 -0400

    setup modal component

---
+++ frontend/src/Components/Record/Modal.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { View, Pressable } from 'react-native'
+import modalStyles from './modalStyles'
+import { PropsWithChildren } from 'react'
+
+type props = PropsWithChildren<{
+    open: boolean
+    close: () => void
+}>
+
+export default function Modal({ children, open, close }: props) {
+    return (
+        <Pressable style={open ? modalStyles.backdrop : modalStyles.invisible} onPress={close}>
+            <View style={open ? modalStyles.modal : modalStyles.invisible} onTouchEnd={e => e.stopPropagation()}>
+                {children}
+            </View>
+        </Pressable>
+    )
+}
